8080d5dcba23c2fd56d2b6a748dfcce04bceba74
iHani <msowe.feha@gmail.com>
2018-04-22 04:35:06 +0300

    more screenshots & link to repo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, List, Segment } from 'semantic-ui-react';
+import { Container, Icon, List, Segment } from 'semantic-ui-react';
 
 const Footer = () => (
   <Segment
@@ -8,8 +8,8 @@ const Footer = () => (
     style={{ margin: '2em 0em 0em', padding: '2em 0em', background: '#0a5094' }} >
     <Container textAlign='center'>
       <List horizontal inverted divided link>
-        <List.Item as='a' href='#'>Site Map</List.Item>
-        <List.Item as='a' href='#'>Contact Us</List.Item>
+        <List.Item as='a' href='https://github.com/iHani/readable-app'><Icon name='github'/></List.Item>
+        <List.Item as='a' href='/'>Home</List.Item>
         <List.Item as='a' href='#'>Terms and Conditions</List.Item>
         <List.Item as='a' href='#'>Privacy Policy</List.Item>
       </List>
